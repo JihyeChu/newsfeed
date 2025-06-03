a63989a364bd74ea0b0cc5c58b1d6244c4db151b
JihyeChu <09strawberry@naver.com>
2025-06-03 17:31:44 +0900

    [feat] 회원가입 API 스웨거 문서화 추가

---
+++ src/main/java/com/sparta/newsfeed/user/controller/docs/UserControllerSwagger.java
@@ -0,0 +1,30 @@
+package com.sparta.newsfeed.user.controller.docs;
+
+import com.sparta.newsfeed.common.dto.ResDTO;
+import com.sparta.newsfeed.user.dto.req.ReqUserPostSignupDTO;
+import com.sparta.newsfeed.user.dto.res.ResUserPostSignupDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "User", description = "회원가입, 로그인 인증 등 인증 관련 API를 제공합니다.")
+@RequestMapping("/api/users")
+public interface UserControllerSwagger {
+
+    @Operation(summary = "회원가입", description = "회원가입을 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "회원가입 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "400", description = "회원가입 실패.", content = @Content(schema = @Schema(implementation = ResDTO.class)))
+    })
+    @PostMapping("/signup")
+    ResponseEntity<ResDTO<ResUserPostSignupDTO>> signup(@Valid @RequestBody ReqUserPostSignupDTO dto);
+
+}
