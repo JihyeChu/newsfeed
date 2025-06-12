0ab7b5edcdbd964b0a6560707d16bf3a467e1c46
JihyeChu <09strawberry@naver.com>
2025-06-12 21:52:56 +0900

    [ feat ] 프로필 수정 응답 DTO 생성

---
+++ src/main/java/com/sparta/newsfeed/user/dto/req/ResUserPatchProfileDTO.java
@@ -0,0 +1,24 @@
+package com.sparta.newsfeed.user.dto.req;
+
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ResUserPatchProfileDTO {
+
+    private String nickname;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$",
+            message = "비밀번호는 8~16자리수여야 합니다. 영문 대소문자, 숫자, 특수문자를 1개 이상 포함해야 합니다.")
+    private String currentPassword;
+
+    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[$@$!%*#?&])[A-Za-z\\d$@$!%*#?&]{8,16}$",
+            message = "비밀번호는 8~16자리수여야 합니다. 영문 대소문자, 숫자, 특수문자를 1개 이상 포함해야 합니다.")
+    private String newPassword;
+
+    private String username;
+
+    private String email;
+}
