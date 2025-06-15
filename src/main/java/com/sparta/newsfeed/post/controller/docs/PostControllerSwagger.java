3a1856133072eb2fd8d9d0a401aa7befd5ed490f
JihyeChu <09strawberry@naver.com>
2025-06-16 01:10:19 +0900

    [ feat ] 게시글 조회 API 스웨거 문서화 추가

--- src/main/java/com/sparta/newsfeed/post/controller/docs/PostControllerSwagger.java
+++ src/main/java/com/sparta/newsfeed/post/controller/docs/PostControllerSwagger.java
@@ -2,6 +2,7 @@ package com.sparta.newsfeed.post.controller.docs;
 
 import com.sparta.newsfeed.common.dto.ResDTO;
 import com.sparta.newsfeed.post.dto.req.ReqPostCreateDTO;
+import com.sparta.newsfeed.post.dto.res.ResPostListDTO;
 import com.sparta.newsfeed.post.res.ResPostCreateDTO;
 import com.sparta.newsfeed.user.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,9 +14,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Post", description = "게시글 생성, 조회, 수정, 삭제 관련 API를 제공합니다.")
 @RequestMapping("/api/posts")
@@ -29,4 +30,11 @@ public interface PostControllerSwagger {
     @PostMapping
     ResponseEntity<ResDTO<ResPostCreateDTO>> createPost(@Valid @RequestBody ReqPostCreateDTO dto, @AuthenticationPrincipal CustomUserDetails userDetails);
 
+    @Operation(summary = "게시글 조회", description = "게시글을 조회 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "게시글 조회 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "400", description = "게시글 조회 실패", content = @Content(schema = @Schema(implementation = ResDTO.class)))
+    })
+    @GetMapping
+    ResponseEntity<ResDTO<List<ResPostListDTO>>> getPostList(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size);
 }
