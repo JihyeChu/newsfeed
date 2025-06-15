424ddc5ae4d14c5e9a6495cc953d9392ac54ed29
JihyeChu <09strawberry@naver.com>
2025-06-15 23:35:11 +0900

    [ feat ] 게시글 생성 API 스웨거 문서화 추가

---
+++ src/main/java/com/sparta/newsfeed/post/controller/docs/PostControllerSwagger.java
@@ -0,0 +1,32 @@
+package com.sparta.newsfeed.post.controller.docs;
+
+import com.sparta.newsfeed.common.dto.ResDTO;
+import com.sparta.newsfeed.post.dto.req.ReqPostCreateDTO;
+import com.sparta.newsfeed.post.res.ResPostCreateDTO;
+import com.sparta.newsfeed.user.security.CustomUserDetails;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Tag(name = "Post", description = "게시글 생성, 조회, 수정, 삭제 관련 API를 제공합니다.")
+@RequestMapping("/api/posts")
+public interface PostControllerSwagger {
+
+    @Operation(summary = "게시글 생성", description = "게시글을 생성 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "게시글 생성 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "400", description = "게시글 생성 실패", content = @Content(schema = @Schema(implementation = ResDTO.class)))
+    })
+    @PostMapping
+    ResponseEntity<ResDTO<ResPostCreateDTO>> createPost(@Valid @RequestBody ReqPostCreateDTO dto, @AuthenticationPrincipal CustomUserDetails userDetails);
+
+}
