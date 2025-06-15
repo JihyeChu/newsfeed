c57f9b84671148e07f0777f039211944b0e666da
JihyeChu <09strawberry@naver.com>
2025-06-15 23:34:54 +0900

    [ feat ] 게시글 생성 요청 DTO

---
+++ src/main/java/com/sparta/newsfeed/post/dto/req/ReqPostCreateDTO.java
@@ -0,0 +1,17 @@
+package com.sparta.newsfeed.post.dto.req;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReqPostCreateDTO {
+
+    @NotBlank(message = "게시글 제목을 입력해주세요.")
+    private String title;
+
+    @NotBlank(message = "게시글 내용을 입력해주세요.")
+    private String content;
+
+}
