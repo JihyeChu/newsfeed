a403b6614fd820476a6dbd02fc082bbf8bd6e29f
JihyeChu <09strawberry@naver.com>
2025-06-15 23:35:23 +0900

    [ feat ] 게시글 생성 응답 DTO

---
+++ src/main/java/com/sparta/newsfeed/post/res/ResPostCreateDTO.java
@@ -0,0 +1,26 @@
+package com.sparta.newsfeed.post.res;
+
+import com.sparta.newsfeed.post.entity.PostEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ResPostCreateDTO {
+
+    private Long id;
+    private String title;
+    private String content;
+
+    public static ResPostCreateDTO of(PostEntity postEntity) {
+        return ResPostCreateDTO.builder()
+                .id(postEntity.getId())
+                .title(postEntity.getTitle())
+                .content(postEntity.getContent())
+                .build();
+    }
+}
