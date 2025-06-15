842823aa2ffd8ae345b358a55df0f60eab8c9388
JihyeChu <09strawberry@naver.com>
2025-06-16 01:37:36 +0900

    [ feat ] 게시글 수정 요청 DTO 생성

---
+++ src/main/java/com/sparta/newsfeed/post/dto/req/ReqPostPatchDTO.java
@@ -0,0 +1,13 @@
+package com.sparta.newsfeed.post.dto.req;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReqPostPatchDTO {
+
+    private String title;
+    private String content;
+
+}
