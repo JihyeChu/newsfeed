40754c89ddea51244c7268c9034096343452dff0
JihyeChu <09strawberry@naver.com>
2025-06-16 01:38:39 +0900

    [ feat ] 게시글 수정 관련 ErrorCode 생성

--- src/main/java/com/sparta/newsfeed/common/exception/ErrorCode.java
+++ src/main/java/com/sparta/newsfeed/common/exception/ErrorCode.java
@@ -13,8 +13,11 @@ public enum ErrorCode {
     INVALID_CURRENT_PASSWORD(HttpStatus.UNAUTHORIZED, 40101, "현재 비밀번호가 일치하지 않습니다."),
     REQUIRED_CURRENT_PASSWORD(HttpStatus.BAD_REQUEST, 40001, "현재 비밀번호를 입력해 주세요."),
     SAME_AS_OLD_PASSWORD(HttpStatus.BAD_REQUEST, 40002, "기존 비밀번호와 동일합니다."),
-    FORBIDDEN_USER_UPDATE(HttpStatus.FORBIDDEN, 40301, "다른 사용자의 프로필은 수정할 수 없습니다.");
+    FORBIDDEN_USER_UPDATE(HttpStatus.FORBIDDEN, 40301, "다른 사용자의 프로필은 수정할 수 없습니다."),
 
+    // POST 도메인 (40020~40039)
+    NOT_FOUND_POST(HttpStatus.NOT_FOUND, 40420, "게시글이 존재하지 않습니다."),
+    FORBIDDEN_POST_UPDATE(HttpStatus.FORBIDDEN, 40320, "다른 사용자의 게시글은 수정할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final Integer code;
