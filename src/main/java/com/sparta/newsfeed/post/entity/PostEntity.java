b5979a81411687ececdebf081e6f210c2ba1986b
JihyeChu <09strawberry@naver.com>
2025-06-15 23:32:59 +0900

    [ feat ] PostEntity 추가

---
+++ src/main/java/com/sparta/newsfeed/post/entity/PostEntity.java
@@ -0,0 +1,45 @@
+package com.sparta.newsfeed.post.entity;
+
+import com.sparta.newsfeed.common.entity.BaseEntity;
+import com.sparta.newsfeed.user.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "posts")
+public class PostEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Builder
+    public PostEntity(String title, String content, UserEntity user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
+
+    public static PostEntity create(String title, String content, UserEntity user) {
+        return PostEntity.builder()
+                .title(title)
+                .content(content)
+                .user(user)
+                .build();
+    }
+}
