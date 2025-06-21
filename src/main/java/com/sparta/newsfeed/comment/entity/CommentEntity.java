dbef17f7e39449b952ad50a9a440f435cf218991
JihyeChu <09strawberry@naver.com>
2025-06-21 18:56:08 +0900

    [ feat ] CommentEntity 생성

---
+++ src/main/java/com/sparta/newsfeed/comment/entity/CommentEntity.java
@@ -0,0 +1,39 @@
+package com.sparta.newsfeed.comment.entity;
+
+import com.sparta.newsfeed.common.entity.BaseEntity;
+import com.sparta.newsfeed.post.entity.PostEntity;
+import com.sparta.newsfeed.user.entity.UserEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "comments")
+public class CommentEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private PostEntity post;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private UserEntity user;
+
+    @Builder
+    public CommentEntity(String content, PostEntity post, UserEntity user) {
+        this.content = content;
+        this.post = post;
+        this.user = user;
+    }
+}
