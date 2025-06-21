6285774f2b73f11857f99b488141404601fad208
JihyeChu <09strawberry@naver.com>
2025-06-21 21:07:48 +0900

    [ feat ] 댓글 수정 응답 DTO

---
+++ src/main/java/com/sparta/newsfeed/comment/dto/req/ReqCommentUpdateDTO.java
@@ -0,0 +1,12 @@
+package com.sparta.newsfeed.comment.dto.req;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ReqCommentUpdateDTO {
+
+    private String content;
+
+}
