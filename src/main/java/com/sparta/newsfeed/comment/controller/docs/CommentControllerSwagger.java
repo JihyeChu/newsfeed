21f9e5c4fdc97c8b8605761f2011cb398c96742e
JihyeChu <09strawberry@naver.com>
2025-06-21 20:15:03 +0900

    [ feat ] 본인이 작성한 전체 댓글 조회 API 스웨거 문서화 추가

--- src/main/java/com/sparta/newsfeed/comment/controller/docs/CommentControllerSwagger.java
+++ src/main/java/com/sparta/newsfeed/comment/controller/docs/CommentControllerSwagger.java
@@ -2,9 +2,8 @@ package com.sparta.newsfeed.comment.controller.docs;
 
 import com.sparta.newsfeed.comment.dto.req.ReqCommentCreateDTO;
 import com.sparta.newsfeed.comment.dto.res.ResCommentCreateDTO;
+import com.sparta.newsfeed.comment.dto.res.ResCommentListDTO;
 import com.sparta.newsfeed.common.dto.ResDTO;
-import com.sparta.newsfeed.post.dto.req.ReqPostCreateDTO;
-import com.sparta.newsfeed.post.dto.res.ResPostCreateDTO;
 import com.sparta.newsfeed.user.security.CustomUserDetails;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -15,10 +14,9 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Tag(name = "Comment", description = "댓글 생성, 조회, 수정, 삭제 관련 API를 제공합니다.")
 @RequestMapping("/api")
@@ -33,4 +31,12 @@ public interface CommentControllerSwagger {
     ResponseEntity<ResDTO<ResCommentCreateDTO>> createComment(@PathVariable Long postId,
                                                               @Valid @RequestBody ReqCommentCreateDTO dto,
                                                               @AuthenticationPrincipal CustomUserDetails userDetails);
+
+    @Operation(summary = "댓글 조회", description = "댓글을 조회 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 조회 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "400", description = "댓글 조회 실패", content = @Content(schema = @Schema(implementation = ResDTO.class)))
+    })
+    @GetMapping("/users/me/comments")
+    ResponseEntity<ResDTO<List<ResCommentListDTO>>> getCommentList(@AuthenticationPrincipal CustomUserDetails userDetails);
 }
