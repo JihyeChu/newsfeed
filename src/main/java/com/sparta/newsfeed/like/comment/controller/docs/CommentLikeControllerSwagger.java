912d4ee472f2b0afe095e370a3eea5d11a741ea0
JihyeChu <09strawberry@naver.com>
2025-07-21 11:39:38 +0900

    [ feat ] 댓글 좋아요 취소 API 스웨거 문서화 추가

--- src/main/java/com/sparta/newsfeed/like/comment/controller/docs/CommentLikeControllerSwagger.java
+++ src/main/java/com/sparta/newsfeed/like/comment/controller/docs/CommentLikeControllerSwagger.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,4 +27,12 @@ public interface CommentLikeControllerSwagger {
     })
     @PostMapping("/{commentId}/likes")
     ResponseEntity<ResDTO<ResCommentLikeCreateDTO>> createCommentLike(@PathVariable Long commentId, @AuthenticationPrincipal CustomUserDetails userDetails);
+
+    @Operation(summary = "댓글 좋아요 취소", description = "댓글 좋아요를 취소 하는 API 입니다.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "댓글 좋아요 취소 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
+            @ApiResponse(responseCode = "400", description = "댓글 좋아요 취소 실패", content = @Content(schema = @Schema(implementation = ResDTO.class)))
+    })
+    @DeleteMapping("/{commentId}/likes")
+    ResponseEntity<ResDTO<Object>> deleteCommentLike(@PathVariable Long commentId, @AuthenticationPrincipal CustomUserDetails userDetails);
 }
