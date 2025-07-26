8b2a086920b26bdba0237773937bffc76b42cc3a
JihyeChu <09strawberry@naver.com>
2025-07-26 20:47:09 +0900

    [ feat ] JwtUtil 에 Refresh Token 생성 및 쿠키 생성 메서드 추가
    - Refresh Token JWT 생성 메서드 추가
    - HttpOnly, Secure 옵션이 적용된 쿠키 생성 메서드 구현

--- src/main/java/com/sparta/newsfeed/common/utils/JwtUtil.java
+++ src/main/java/com/sparta/newsfeed/common/utils/JwtUtil.java
@@ -4,6 +4,7 @@ import com.sparta.newsfeed.user.security.CustomUserDetailsService;
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.Cookie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -18,6 +19,8 @@ import java.util.Date;
 @Slf4j(topic = "JwtUtil")
 @Component
 public class JwtUtil {
+    private static final String REFRESH_HEADER = "Refresh-jwt";
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 60 * 60 * 1000L * 24 * 7; // 7일
     private static final long ACCESS_TOKEN_EXPIRE_TIME = 60 * 60 * 1000L * 24; // 24시간
     private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
     private final CustomUserDetailsService customUserDetailsService;
@@ -48,6 +51,30 @@ public class JwtUtil {
                 .signWith(key, signatureAlgorithm)
                 .compact();
 
+    }
+
+    // JWT Refresh Token 생성
+    public String generateRefreshToken(String nickname) {
+        Date date = new Date();
+
+        return Jwts.builder()
+                .setSubject(nickname)
+                .setExpiration(new Date(date.getTime() + REFRESH_TOKEN_EXPIRE_TIME))
+                .setIssuedAt(date)
+                .signWith(key, signatureAlgorithm)
+                .compact();
+
+    }
+
+    public Cookie generateRefreshJwtCookie(String refreshToken) {
+        Cookie refreshJwtCookie = new Cookie(REFRESH_HEADER, refreshToken);
+
+        refreshJwtCookie.setHttpOnly(true);
+        refreshJwtCookie.setSecure(true);
+        refreshJwtCookie.setPath("/");
+        refreshJwtCookie.setMaxAge((int) REFRESH_TOKEN_EXPIRE_TIME);
+
+        return refreshJwtCookie;
     }
 
     // 토큰에서 아이디 추출
