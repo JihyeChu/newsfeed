2fea4b21590e337d651dbe24a12b29aae67ce995
JihyeChu <09strawberry@naver.com>
2025-07-27 16:08:45 +0900

    [ refactor ] Access Token 검증 필터 로직 메서드로 분리 및 수정

--- src/main/java/com/sparta/newsfeed/common/filter/JwtFilter.java
+++ src/main/java/com/sparta/newsfeed/common/filter/JwtFilter.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -22,31 +23,35 @@ public class JwtFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         String requestURI = request.getRequestURI();
-        String jwt = null;
 
-        String authorizationHeader = request.getHeader("Authorization");
-
-        if (requestURI.equals("/api/users/signup") || requestURI.equals("/api/users/login")) {
+        if (requestURI.equals("/api/users/signup") || requestURI.equals("/api/users/login") || requestURI.equals("/api/users/token/refresh-token")) {
             filterChain.doFilter(request, response);
             return;
         }
 
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-            log.info("JWT 토큰이 필요 합니다.");
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT 토큰이 필요 합니다.");
+        String accessToken = checkAccessToken(request);
+
+        // Access Token 이 유효할 경우
+        if (accessToken != null && jwtUtil.validateToken(accessToken)) {
+            setAuthentication(accessToken);
+        } else {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "JWT 토큰이 유효하지 않거나, 만료되었습니다.");
             return;
         }
 
-        jwt = authorizationHeader.substring(7);
-
-        if (jwtUtil.validateToken(jwt)) {
-            // 토큰으로부터 인증 정보(Authentication) 객체 생성
-            Authentication authentication = jwtUtil.getAuthentication(jwt);
-
-            // SecurityContext에 인증 정보를 저장해, 이후 인증된 사용자로 인식되도록 함
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-        }
-
         filterChain.doFilter(request, response);
+    }
+
+    private void setAuthentication(String accessToken) {
+        Authentication authentication = jwtUtil.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+    }
+
+    private String checkAccessToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (StringUtils.hasText(header)) {
+            return header;
+        }
+        return null;
     }
 }
