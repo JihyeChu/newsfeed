ecb0d0d8872d77b6680bd48600a4536dc55b1dd2
JihyeChu <09strawberry@naver.com>
2025-08-07 19:19:02 +0900

    [ config ] FeignClient 활성화를 위한 @EnableFeignClients 추가

--- src/main/java/com/sparta/newsfeed/NewsfeedApplication.java
+++ src/main/java/com/sparta/newsfeed/NewsfeedApplication.java
@@ -2,8 +2,10 @@ package com.sparta.newsfeed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.sparta.newsfeed.post")
 public class NewsfeedApplication {
 
     public static void main(String[] args) {
